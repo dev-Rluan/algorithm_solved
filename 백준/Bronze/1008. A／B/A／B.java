7f3b1d30828167d1ad3b0fc8adc52bfce8888515
Rluan <73861946+dev-Rluan@users.noreply.github.com>
2024-03-09 15:35:45 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 18448 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		double c = (double)a/b;
+		System.out.println(c);
+	}
+}
